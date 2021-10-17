449216d4fb0192ffc3d6fb5267367e787d74f1e0
DmtDimitrov <dmt.dimitrov@gmail.com>
2021-10-17 16:01:41 +0300

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,2 +1,12 @@
+const express = require('express');
+
+const { PORT } = require('./constants');
 const expressConfig = require('./config/express.js');
+
+
+const app = express();
+
 expressConfig(app);
+
+
+app.listen(PORT, () => console.log(`The app is running on http://localhost:${PORT}/...`));

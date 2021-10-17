54e6f44d51c3673ac6b96421500681904dc64cae
DmtDimitrov <dmt.dimitrov@gmail.com>
2021-10-17 15:58:12 +0300

    add constants folder and file
    setup constants

---
+++ src/constants/index.js
@@ -0,0 +1,5 @@
+exports.PORT = 5000;
+exports.DB_URI = 'mongodb://localhost:27017/js-backend-exam';
+exports.SALT_ROUNDS = 10;
+exports.TOKEN_SECRET = 'FAF99AB68212CD88654D49C3B865C';
+exports.AUTH_COOKIE_NAME = 'authAppToken';

d90626178bed7c998ad7ad8d7c11ff92287b798b
DmtDimitrov <dmt.dimitrov@gmail.com>
2021-10-18 05:25:54 +0300

    Create tokenService.js

---
+++ src/services/tokenService.js
@@ -0,0 +1,19 @@
+const jwt = require('../utils/jwt.js');
+const { TOKEN_SECRET } = require('../constants');
+
+exports.createToken = async(user) => {
+    try {
+        //TODO: adapt parameters to project requirements
+        let payload = {
+            _id: user._id,
+            name: user.name,
+            username: user.username,
+        };
+
+        let token = await jwt.sign(payload, TOKEN_SECRET);
+        return token;
+
+    } catch (error) {
+        console.log(error);
+    }
+};

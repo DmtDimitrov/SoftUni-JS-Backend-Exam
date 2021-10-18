d3e4f77d6b61e794abe3783cb35a92b41c5d5cb7
DmtDimitrov <dmt.dimitrov@gmail.com>
2021-10-18 05:27:38 +0300

    Create database.js

---
+++ src/config/database.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const { DB_URI } = require('../constants');
+
+// async function databaseConfig() {
+//     await mongoose.connect(DB_URI);
+//     console.log('Database connected');
+// };
+
+exports.databaseConfig = function() {
+    return mongoose.connect(DB_URI);
+};
+
+// module.exports = databaseConfig;

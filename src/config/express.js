6ee558b46f5826e0b7f867960a8546fab3ea037b
DmtDimitrov <dmt.dimitrov@gmail.com>
2021-10-18 05:27:34 +0300

    Update express.js

--- src/config/express.js
+++ src/config/express.js
@@ -3,17 +3,54 @@ const handlebars = require('express-handlebars');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 
-function expressConfig(app) {
+const { authMiddleware } = require('../middlewares/authMiddleware.js')
+
+exports.handlebarsConfig = function(app) {
     app.engine('hbs', handlebars({
         extname: 'hbs'
     }));
     app.set('views', path.resolve(__dirname, '../views'));
     app.set('view engine', 'hbs')
+}
 
+exports.expressConfig = function(app) {
     app.use('/static', express.static(path.resolve(__dirname, '../public')));
     app.use(express.urlencoded({ extended: true }));
     app.use(cookieParser());
+    app.use(authMiddleware);
     //TODO: add storage middlewares
 };
 
-module.exports = expressConfig;
+// function handlebarsConfig(app) {
+//     app.engine('hbs', handlebars({
+//         extname: 'hbs'
+//     }));
+//     app.set('views', path.resolve(__dirname, '../views'));
+//     app.set('view engine', 'hbs')
+// };
+
+// function expressConfig(app) {
+//     app.use('/static', express.static(path.resolve(__dirname, '../public')));
+//     app.use(express.urlencoded({ extended: true }));
+//     app.use(cookieParser());
+//     //TODO: add storage middlewares
+// };
+
+// exports.handlebarsConfig = handlebarsConfig;
+// exports.expressConfig = expressConfig;
+
+
+// function expressConfig(app) {
+//     app.engine('hbs', handlebars({
+//         extname: 'hbs'
+//     }));
+//     app.set('views', path.resolve(__dirname, '../views'));
+//     app.set('view engine', 'hbs')
+
+//     app.use('/static', express.static(path.resolve(__dirname, '../public')));
+//     app.use(express.urlencoded({ extended: true }));
+//     app.use(cookieParser());
+//     //TODO: add storage middlewares
+// };
+
+// module.exports = expressConfig;
